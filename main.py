acee5e4087a1fae6660ebdb25968863853a7b6b4
Hashmi_Abdullah <abdullahhashmi005@gmail.com>
2023-06-26 21:44:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,53 +16,55 @@ key = st.sidebar.text_input(
     type="password")
 
  # Read Data as Pandas
-data = pd.read_csv('3states.csv')
+data = pd.read_csv('data/3states.csv')
 
 # Check if key is entered
 if key:
     os.environ['OPENAI_API_KEY'] = key
+    try:
+        agent = create_pandas_dataframe_agent(OpenAI(
+            temperature=0.0),data, verbose=True)
 
-    agent = create_pandas_dataframe_agent(OpenAI(
-        temperature=0.0),data, verbose=True)
+        if 'history' not in st.session_state:
+            st.session_state['history'] = []
 
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+        # Define Generated and Past Chat Arrays
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = []
 
-    # Define Generated and Past Chat Arrays
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = []
+        if 'past' not in st.session_state:
+            st.session_state['past'] = []
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = []
+        # Accept input from user
+        query = st.text_input("Enter a query:")
 
-    # Accept input from user
-    query = st.text_input("Enter a query:")
+        # container for the chat history
+        response_container = st.container()
+        # container for the user's text input
+        container = st.container()
 
-    # container for the chat history
-    response_container = st.container()
-    # container for the user's text input
-    container = st.container()
+        # Execute Button Logic
+        if st.button("Execute") and query:
+            with st.spinner('Generating response...'):
+                try:
+                    answer = agent.run(query)
+                    st.session_state['history'].append((query, answer))
 
-    # Execute Button Logic
-    if st.button("Execute") and query:
-        with st.spinner('Generating response...'):
-            try:
-                answer = agent.run(query)
-                st.session_state['history'].append((query, answer))
-
-                # Store conversation
-                st.session_state.past.append(query)
-                st.session_state.generated.append(answer)
-
-
-                # Display conversation in reverse order
-                if st.session_state['generated']:
-                    with response_container:
-                        for i in range(len(st.session_state['generated'])):
-                            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user',
-                                    avatar_style="big-smile")
-                            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                    # Store conversation
+                    st.session_state.past.append(query)
+                    st.session_state.generated.append(answer)
 
 
-            except Exception as e:
-                st.error(f"An error occurred: {str(e)}")
+                    # Display conversation in reverse order
+                    if st.session_state['generated']:
+                        with response_container:
+                            for i in range(len(st.session_state['generated'])):
+                                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user',
+                                        avatar_style="big-smile")
+                                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+
+
+                except Exception as e:
+                    st.error(f"An error occurred: {str(e)}")
+    except:
+        st.error(f"Token Limit Excided")
